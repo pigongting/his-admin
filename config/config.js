4012c9fe8d645a93fed0e7c2febe26ec3270a65e
pigongting <466548808@qq.com>
2017-10-23 17:19:06 +0800

    config

--- config/config.js
+++ config/config.js
@@ -12,16 +12,4 @@ module.exports = {
   CORS: [],
   openPages: ['/login'],
   apiPrefix: '/api/v1',
-  api: {
-    userLogin: `${APIV1}/user/login`,
-    userLogout: `${APIV1}/user/logout`,
-    userInfo: `${APIV1}/userInfo`,
-    users: `${APIV1}/users`,
-    posts: `${APIV1}/posts`,
-    user: `${APIV1}/user/:id`,
-    dashboard: `${APIV1}/dashboard`,
-    menus: `${APIV1}/menus`,
-    v1test: `${APIV1}/test`,
-    v2test: `${APIV2}/test`,
-  },
 };
